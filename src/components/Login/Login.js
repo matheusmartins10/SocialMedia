21fb67315819205a8ec617dad647299da5e0e7fa
Matheus Martins <mmartins.oliveira@outlook.com>
2020-10-31 23:18:50 -0300

    Login Routes

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
+import { Routes, Route } from 'react-router-dom'
+
+import LoginCreate from './LoginCreate'
+import LoginForm from './LoginForm'
+import LoginPasswordReset from './LoginPasswordReset'
+import LoginPasswordLost from './LoginPasswordLost'
+
 const Login = () => {
     return (
         <div>
-            Login
+            <Routes>
+                <Route path="/" element={<LoginForm />} />
+                <Route path="/criar" element={<LoginCreate />} />
+                <Route path="/perdeu" element={<LoginPasswordLost />} />
+                <Route path="/resetar" element={ <LoginPasswordReset /> } />
+            </Routes>
         </div>
     )
 }
